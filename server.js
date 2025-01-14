04cf1577df330c783fe9bd43231783e829ef34ef
Anantkumar <anantkumarpujar04@gmail.com>
2025-01-15 02:13:11 +0530

    updated pg credentials

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT;
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST ,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
 });
